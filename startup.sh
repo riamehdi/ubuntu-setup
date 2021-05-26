cf154bfca8f93832f82041e03f61ad04480b574e
KHADIR <m@khadir.net>
2021-05-26 09:15:51 +0200

    just want i need right now with bash profile

--- startup.sh
+++ startup.sh
@@ -4,21 +4,21 @@ echo "Welcome! Let's start setting up your system xD It could take more than 10 
 
 sudo apt-get update
 
-echo 'installing curl' 
+echo 'installing curl'
 sudo apt install curl -y
 
 echo 'installing vim'
 sudo apt install vim -y
 clear
 
-echo 'installing git' 
+echo 'installing git'
 sudo apt install git -y
 
 echo "What name do you want to use in GIT user.name?"
 echo "For example, mine will be \"Olavio Lacerda\""
 read git_config_user_name
 git config --global user.name "$git_config_user_name"
-clear 
+clear
 
 echo "What email do you want to use in GIT user.email?"
 echo "For example, mine will be \"olavio.lacerda@hotmail.com\""
@@ -39,7 +39,7 @@ ssh-keygen -t rsa -b 4096 -C $git_config_user_email
 ssh-add ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub | xclip -selection clipboard
 
-echo 'enabling workspaces for both screens' 
+echo 'enabling workspaces for both screens'
 gsettings set org.gnome.mutter workspaces-only-on-primary false
 
 echo 'installing zsh'
@@ -52,27 +52,9 @@ sudo apt-get install xclip -y
 
 export alias pbcopy='xclip -selection clipboard'
 export alias pbpaste='xclip -selection clipboard -o'
-source ~/.zshrc
+source ~/.bashrc
 
-echo 'installing code'
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get install apt-transport-https -y
-sudo apt-get update
-sudo apt-get install code -y # or code-insiders
-
-echo 'installing extensions'
-code --install-extension Shan.code-settings-sync
-
-echo 'installing spotify' 
-snap install spotify
-
-echo 'installing chrome' 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-
-echo 'installing nvm' 
+echo 'installing nvm'
 sh -c "$(curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash)"
 
 export NVM_DIR="$HOME/.nvm" && (
@@ -84,165 +66,15 @@ git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --
 echo '\nexport PATH=/usr/local/share/npm/bin:$PATH\n'
 
 echo '\nexport NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"\n' >> .zshrc
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"\n' >> .bashrc
 
-source ~/.zshrc
+source ~/.bashrc
 nvm --version
 nvm install 16
 nvm alias default 16
 node --version
 npm --version
 
-echo 'installing Typescript'
-npm install -g typescript
-
-echo 'installing autosuggestions' 
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
-source ~/.zshrc
-
-echo 'installing theme'
-sudo apt install fonts-firacode -y
-wget -O ~/.oh-my-zsh/themes/node.zsh-theme https://raw.githubusercontent.com/skuridin/oh-my-zsh-node-theme/master/node.zsh-theme 
-sed -i 's/.*ZSH_THEME=.*/ZSH_THEME="node"/g' ~/.zshrc
-
-echo 'installing franz' 
-wget https://github.com/meetfranz/franz/releases/download/v5.1.0/franz_5.1.0_amd64.deb -O franz.deb
-sudo dpkg -i franz.debchristian-kohler.path-intellisense
-sudo apt-get install -y -f 
-
-echo 'installing terminator'
-sudo apt-get update
-sudo apt-get install terminator -y
-
-echo 'adding dracula theme' 
-cat <<EOF >  ~/.config/terminator/config
-[global_config]
-  title_transmit_bg_color = "#ad7fa8"
-[keybindings]
-  close_term = <Primary>w
-  close_window = <Primary>q
-  new_tab = <Primary>t
-  new_window = <Primary>i
-  paste = <Primary>v
-  split_horiz = <Primary>e
-  split_vert = <Primary>d
-  switch_to_tab_1 = <Primary>1
-  switch_to_tab_10 = <Primary>0
-  switch_to_tab_2 = <Primary>2
-  switch_to_tab_3 = <Primary>3
-  switch_to_tab_4 = <Primary>4
-  switch_to_tab_5 = <Primary>5
-  switch_to_tab_6 = <Primary>6
-[layouts]
-  [[default]]
-    [[[child1]]]
-      parent = window0
-      type = Terminal
-    [[[window0]]]
-      parent = ""
-      type = Window
-[plugins]
-[profiles]
-  [[default]]
-    cursor_color = "#aaaaaa"
-EOF
-
-
-cat <<EOF >>  ~/.config/terminator/config
-[[Dracula]]
-    background_color = "#1e1f29"
-    background_darkness = 0.88
-    background_type = transparent
-    copy_on_selection = True
-    cursor_color = "#bbbbbb"
-    foreground_color = "#f8f8f2"
-    palette = "#000000:#ff5555:#50fa7b:#f1fa8c:#bd93f9:#ff79c6:#8be9fd:#bbbbbb:#555555:#ff5555:#50fa7b:#f1fa8c:#bd93f9:#ff79c6:#8be9fd:#ffffff"
-    scrollback_infinite = True
-EOF
-
-echo 'installing docker' 
-sudo apt-get remove docker docker-engine docker.io
-sudo apt install docker.io -y
-sudo systemctl start docker
-sudo systemctl enable docker
-docker --version
-
-chmod 777 /var/run/docker.sock
-docker run hello-world
-
-echo 'installing docker-compose' 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
-
-echo 'installing fzf'
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --all
-source ~/.zshrc
-
-echo 'installing dbeaver'
-wget -c https://dbeaver.io/files/6.0.0/dbeaver-ce_6.0.0_amd64.deb
-sudo dpkg -i dbeaver-ce_6.0.0_amd64.deb
-sudo apt-get install -f
-
-echo 'installing Postman' 
-snap install postman
-
-echo 'installing Redocs'
-sudo npm install -g @redocly/openapi-cli
-
-echo 'installing Golang'
-wget -c https://golang.org/dl/go1.16.3.linux-amd64.tar.gz
-shasum -a 256 go1.16.3.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.16.3.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-go version
-sudo mkdir ~/go
-sudo cat <<EOF >> ~/.zshrc
-export GOPATH=$HOME/go
-export PATH=$GOPATH/bin:$PATH
-EOF
-
-echo 'commiting changes'
-source ~/.zshrc
-sudo dpkg --configure -a 
-sudo apt-get update --fix-missing
-sudo apt-get autoremove
-
-clear 
-
-echo 'Do you wanna install extra content? (y/N)'
-read extra_content
-if echo "$extra_content" | grep -iq "^y" ;then
-	echo 'installing vlc'
-  sudo apt install vlc -y
-  sudo apt install vlc-plugin-access-extra libbluray-bdj libdvdcss2 -y
-
-  echo 'installing transmission'
-  sudo add-apt-repository ppa:transmissionbt/ppa
-  sudo apt-get update
-  sudo apt-get install transmission transmission-qt -y
-
-  echo 'installing Robo3t'
-  snap install robo3t-snap
-
-  echo 'installing kubectl'
-  curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-
-  echo 'installing heroku-cli'
-  curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
-  heroku --version
-
-  echo 'installing aws-cli' 
-  sudo apt-get install awscli -y
-  aws --version
-  curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
-  sudo dpkg -i session-manager-plugin.deb
-  session-manager-plugin --version
-else
-	echo "Okay, no problem. :) Let's finish it then!"
-fi
-
+clear
 
 echo 'All tools successfully installed. You are ready to shine :).'
