2b1eaec9a19dcc5a0f76f1539865fb2769369e7f
M. KHADIR <contact@khadir.net>
2021-07-14 11:22:17 +0100

    add ifconfig comman

--- startup.sh
+++ startup.sh
@@ -16,6 +16,9 @@ sudo apt install ssh
 sudo systemctl enable --now ssh
 sudo systemctl status ssh
 
+echo 'installing command ifcondif'
+sudo apt install net-tools
+
 echo 'installing AnyDesk'
 wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
 echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
