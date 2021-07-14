49d4c40cf36e1795cca59dfd860ad2c8029fb304
M. KHADIR <contact@khadir.net>
2021-07-14 11:07:52 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -11,6 +11,17 @@ echo "export LD_LIBRARY_PATH=${HOME}/libs/" >> .bashrc
 source ~/.bashrc
 echo $LD_LIBRARY_PATH
 
+echo 'installing SSH'
+sudo apt install ssh
+sudo systemctl enable --now ssh
+sudo systemctl status ssh
+
+echo 'installing AnyDesk'
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
+echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
+sudo apt update
+sudo apt install anydesk
+
 echo 'installing curl'
 sudo apt install curl -y
 
